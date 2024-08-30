01b037b08732a06cd37bd6ea5ebd583343eb51d0
pwolthausen <pwolthausen@cloudops.com>
2024-08-30 13:11:48 -0400

    Add data source for AMI

--- aws/workEnv/main.tf
+++ aws/workEnv/main.tf
@@ -6,3 +6,16 @@
 #   environment        = "pw-test"
 #   access_ips         = ["142.115.192.104"]
 # }
+
+data "aws_ami" "windows-2019" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "platform"
+    values = ["windows"]
+  }
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2022-English-Full-Base*"]
+  }
+}
